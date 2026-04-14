b8d6cbdeb923079136cd5946eedfc069f4fb4201
los martes no hay luna <serseomadrid5@gmail.com>
2026-04-14 08:04:58 +0200

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     attributes: {
       NOMBRE:           nombre,
       EMPRESA:          empresa,
-      TELEFONO:         telefono,
+      SMS:              telefono,
       SECTOR:           sector    || '',
       PRESENTA_NEGOCIO: presenta  || ''
     },
